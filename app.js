114ac3aaf0a5c18b8589536f477f095aa654d43f
black-RAM <pplanes311@gmail.com>
2025-08-22 18:44:09 +0300

    use passport.initialize() middleware to fix issue

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
