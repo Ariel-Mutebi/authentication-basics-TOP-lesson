d94fe6d0dc245ca88284dfa8c8f1eac9ecbdb061
black-RAM <pplanes311@gmail.com>
2025-08-22 23:11:12 +0300

    removed passport initialise and it still works

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
